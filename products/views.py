c56c71ea32ecf2d71d8f32490604c5af18658216
checho rios <chechorios2008@gmail.com>
2023-10-05 00:04:18 -0500

    "Creación de templates y app Accounts"

--- products/views.py
+++ products/views.py
@@ -5,10 +5,10 @@ from .models import Product
 def index(request):
     products = Product.objects.all()
     print(products)
-    return render(request, 'list_of_products.html', 
+    return render(request, 'products/list_of_products.html', 
                   {'products': products})
     
 def get_product(request, id):
     product = Product.objects.get(id=id)
-    return render(request, 'show_product.html',
+    return render(request, 'products/show_product.html',
                   {'product':product})
